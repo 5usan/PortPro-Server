b8a1391e2f979257510ff60f232620575172ba2f
5usan <susanshrestha2056@gmail.com>
2022-08-19 17:23:55 +0545

    added mongoose and database connecte

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
+import databaseConfig from "./configs/databaseConfig.js";
 import "dotenv/config";
-
 const app = express();
 app.use(express.json());
+databaseConfig();
 
 app.get("/", (req, res) => {
   res.send("Port-Pro Running Properly. Have a Wonderful Day.");
@@ -12,4 +13,4 @@ app.listen(process.env.PORT, () => {
   console.log(
     `server started at port ${process.env.BASE_URL}${process.env.PORT}`
   );
-});
+});  
